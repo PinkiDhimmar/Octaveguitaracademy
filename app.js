e17157a2242b24002d935a0aee3b9988f5009d4d
PinkiDhimmar <Pinkidhimmar@gmail.com>
2025-05-13 15:50:07 +1200

    adding

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express'); 
+const app = express(); 
+ 
+app.use('/public', express.static('public'));
+
+app.set('view engine','ejs');
+app.get('/', function (req, res){
+  res.render("homepage");
+ });
+
+
+ 
+//app.get('/about', (req, res) => { 
+ // res.send('<h2>About This Project</h2><p>This is a sample Express app.</p>'); 
+//}); 
+ 
+app.listen(3000, () => { 
+  console.log('Server is running on http://localhost:3000'); 
+}); 
